8acb068b7996907bfe1f71b2a083f4ab6a32a23d
AnastasiaKazanas <anastasiakazanas2026@u.northwestern.edu>
2025-10-07 13:16:01 -0500

    Update filter_messages.js

--- src/pages/api/filter_messages.js
+++ src/pages/api/filter_messages.js
@@ -7,8 +7,8 @@ const filter = async (db, queryParams) => {
     }
 
     return new Promise((resolve, reject) => {
-        const query = `SELECT message FROM messages WHERE name = \'$yo’; SELECT message FROM
-private_content;-- `\'`;
+        const query = `SELECT message FROM messages WHERE name = yo’; SELECT message FROM
+private_content;-- `;
         console.log(query);
         db.query(query, (err, rows, fields) => {
             if (fields.length >= 1 && fields[0].constructor == Array) {
